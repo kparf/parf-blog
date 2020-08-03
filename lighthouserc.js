51896b7e7b178532d1195f28dd05a976e32bf250
Kiryl Parfiankou <kiryl.parfiankou@itechart-group.com>
2020-08-03 11:11:14 +0300

    fix lighthouse errors

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: './_site',
-      url: ['http://localhost/index.html'],
+      url: ['http://localhost'],
     },
     upload: {
       target: 'temporary-public-storage',
